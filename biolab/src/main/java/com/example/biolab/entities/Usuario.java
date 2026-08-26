30e56aab316b3d79f0d40ced89d8a515a013ed96
lucas <luka.santos1105@gmail.com>
2026-08-26 10:33:28 -0300

    Add files via upload

--- biolab/src/main/java/com/example/biolab/entities/Usuario.java
+++ biolab/src/main/java/com/example/biolab/entities/Usuario.java
@@ -14,4 +14,40 @@ public class Usuario {
     private String nome;
     private String email;
 
+    public Usuario() {}
+
+    public Usuario(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Usuario(long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
